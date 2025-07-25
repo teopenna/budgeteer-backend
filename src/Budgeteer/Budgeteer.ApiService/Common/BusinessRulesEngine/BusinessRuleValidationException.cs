b826a280a21fba636430a039642a467087580aff
Matteo Penna <penna.matteo@gmail.com>
2025-07-26 00:26:57 +0200

    Add business rules base files

---
+++ src/Budgeteer/Budgeteer.ApiService/Common/BusinessRulesEngine/BusinessRuleValidationException.cs
@@ -0,0 +1,8 @@
+﻿namespace Budgeteer.ApiService.Common.BusinessRulesEngine;
+
+internal class BusinessRuleValidationException : InvalidOperationException
+{
+    internal BusinessRuleValidationException(string message) : base(message)
+    {
+    }
+}
---
+++ src/Budgeteer/Budgeteer.ApiService/Common/BusinessRulesEngine/IBusinessRule.cs
@@ -0,0 +1,7 @@
+﻿namespace Budgeteer.ApiService.Common.BusinessRulesEngine;
+
+internal interface IBusinessRule
+{
+    bool IsMet();
+    string Error { get; }
+}
