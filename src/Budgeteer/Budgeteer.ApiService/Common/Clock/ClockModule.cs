827dc480f3bf570db802e807f6729fbd35a20b6c
Matteo Penna <penna.matteo@gmail.com>
2025-07-26 00:27:19 +0200

    Add clock module

---
+++ src/Budgeteer/Budgeteer.ApiService/Common/Clock/ClockModule.cs
@@ -0,0 +1,7 @@
+﻿namespace Budgeteer.ApiService.Common.Clock;
+
+internal static class ClockModule
+{
+    internal static IServiceCollection AddClock(this IServiceCollection services) =>
+        services.AddSingleton(TimeProvider.System);
+}
